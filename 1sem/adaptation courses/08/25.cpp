4744ebb565fc05f822a339de1c03edeb831b3e11
Andrew <mrcluster@ya.ru>
2019-11-18 11:02:11 +0700

    c2

--- 1sem/adaptation courses/08/25.cpp
+++ 1sem/adaptation courses/08/25.cpp
@@ -3,11 +3,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 using namespace std;
 
 vector<int> max_diff(const vector<int> &vec) {
     vector<int> v = vec;
-    sort(begin(v), end(v));\
+    sort(begin(v), end(v));
     swap(*(v.end() - 1), v[1]);
     v.resize(2);
     return v;
@@ -19,12 +20,13 @@ int main() {
     cin >> N;
     vector<int> v;
     v.reserve(N);
-    cout << "v[...] >>>";
+    srand(time(0));
     for (int i = 0; i < N; i++) {
-        int j;
-        cin >> j;
+        int j = -5 + rand() % 10;
+        cout << j << " ";
         v.push_back(j);
     }
+    cout << endl;
     vector<int> nv = max_diff(v);
     cout << "Biggest diff is between " << nv[0] << " and " << nv[1] << " (eq. " << nv[0] - nv[1] << ")" << endl;
     return 0;
